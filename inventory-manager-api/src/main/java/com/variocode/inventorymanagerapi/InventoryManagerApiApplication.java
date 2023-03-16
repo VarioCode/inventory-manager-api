105ac5cbd3085d3b1c69401a9e53bd5068f9115b
JMC-Cyber <jmccrans@gmail.com>
2023-03-16 09:57:23 +0100

    started

---
+++ inventory-manager-api/src/main/java/com/variocode/inventorymanagerapi/InventoryManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.variocode.inventorymanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagerApiApplication.class, args);
+    }
+
+}
---
+++ inventory-manager-api/src/test/java/com/variocode/inventorymanagerapi/InventoryManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.variocode.inventorymanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
